0c35946eb9e940184d33cd5080d05c070f336843
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-10-21 19:51:56 -0400

    feat: explosion now applies to dummies

--- quinn-things/grenade.gd
+++ quinn-things/grenade.gd
@@ -5,6 +5,8 @@ var vertical_speed = 200.0
 var gravity = 400.0
 var has_bounced: bool = false
 var explosion_timer: Timer
+var explode: bool = false
+@export var explosion_radius: float = 1000
 
 func start(p: Vector2, r: float):
 	global_position = p
@@ -17,12 +19,27 @@ func _ready() -> void:
 	explosion_timer = Timer.new()
 	explosion_timer.one_shot = true
 	get_tree().root.add_child(explosion_timer)
-	explosion_timer.timeout.connect(self._go_boom)
+	explosion_timer.timeout.connect(self._set_explode)
 	explosion_timer.start(1)
 
 func _physics_process(_delta):
-	pass
+	if explode:
+		_go_boom()
 
 func _go_boom() -> void:
-	print("boom")
+	var query: PhysicsShapeQueryParameters2D = PhysicsShapeQueryParameters2D.new()
+	query.collide_with_areas = true
+	var shape: Shape2D = CircleShape2D.new()
+	shape.radius = explosion_radius
+	query.shape = shape
+	query.exclude = [self]
+	
+	var results: Array[Dictionary] = get_world_2d().direct_space_state.intersect_shape(query)
+	for result in results:
+		var obj: Object = result.collider
+		if obj.has_method("apply_impulse"):
+			obj.apply_impulse((obj.global_position - global_position) * 3)
 	queue_free()
+
+func _set_explode() -> void:
+	explode = true
