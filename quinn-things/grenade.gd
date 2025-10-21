fdeaa97f722c57c02b8af91f4a9e7fef720c33af
Quinn Donnelly <quinndonnelly22@gmail.com>
2025-10-21 14:03:03 -0400

    feat: fix gernade timer add to tree

--- quinn-things/grenade.gd
+++ quinn-things/grenade.gd
@@ -16,13 +16,13 @@ func _ready() -> void:
 	linear_damp = 2
 	explosion_timer = Timer.new()
 	explosion_timer.one_shot = true
+	get_tree().root.add_child(explosion_timer)
 	explosion_timer.timeout.connect(self._go_boom)
 	explosion_timer.start(1)
 
-func _physics_process(delta):
-	vertical_speed -= gravity * delta
-	if vertical_speed <= 0 and not has_bounced:
-		# Simulate hitting the ground
-		apply_central_impulse(Vector2(linear_velocity.x, linear_velocity.y) * 0.2)
-		has_bounced = true
+func _physics_process(_delta):
+	pass
 
+func _go_boom() -> void:
+	print("boom")
+	queue_free()
